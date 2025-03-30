1fe6d200e5900d0cbaaff732f51bae1c11828f7b
mnek1t <mednikita2004@gmail.com>
2025-03-30 13:32:41 +0300

    make boxes of equal size and adjust styling

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -6,16 +6,16 @@ const Login = () => {
         <div className='login-main-container'>
             <h3>Connect using ...</h3>
             <div className="login-options">
-                <a href='/smartid' className='option-smart-id'>
-                    <img src={smartIdLogo} alt=''></img>
+                <a href='/smartid' className='option-smart-id option'>
+                    <img src={smartIdLogo} alt='smartid'></img>
                 </a>
-                <a href='/eparaksts' className='option-eParaksts'>
-                    <img src={eParakstsLogo} alt=''></img>
+                <a href='/eparaksts' className='option-eParaksts option'>
+                    <img src={eParakstsLogo} alt='eparaksts'></img>
                 </a>
-                <a href='/basic' className='option-basic-authorization'>
+                <a href='/basic' className='option-basic-authorization option'>
                     Basic Authorization
                 </a>
-                <a href='/inbank' className='option-interenet-banking'>
+                <a href='/inbank' className='option-interenet-banking option'>
                     Internet Banking Authentication
                 </a>
             </div>
