23003940eb8cc775f88b4157085d3d9494f0f89f
mnek1t <mednikita2004@gmail.com>
2025-03-30 13:31:46 +0300

    add Contact components as footer

--- frontend/src/pages/BasicLoginPage.tsx
+++ frontend/src/pages/BasicLoginPage.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header/Header";
 import BasicLogin from "../components/BasicLogin/BasicLogin";
+import Contact from "../components/Contact/Contact";
 const BasicLoginPage = () => {
     return(
     <div className="app-container">
         <Header/>
         <hr/>
         <BasicLogin/>
+        <Contact/>
     </div>
     );
 }
