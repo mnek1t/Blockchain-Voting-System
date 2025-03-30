2f3483bcfa94d15debb424b366cacd7193755a4b
mnek1t <mednikita2004@gmail.com>
2025-03-30 02:21:25 +0200

    add useful infromation. copy from eds

---
+++ frontend/src/components/UsefulInformation/UsefulInformation.tsx
@@ -0,0 +1,35 @@
+import usefulData from "./usefulData";
+interface LinkData {
+    url: string;
+    label: string;
+  }
+
+const UsefulInformation = () => {
+    const renderTextWithInternalReferences = (text: string, links: LinkData[] | null) => {
+        if(!links || links.length === 0) return text;
+        let updText = text;
+        links.forEach(link => {
+            const regex = new RegExp(`<a>${link.label}</a>`, 'g');
+            updText = updText.replace(regex, `<a href="${link.url}" target="_blank" rel="noopener noreferrer">${link.label}</a>`);
+        })
+
+        return <span dangerouslySetInnerHTML={{ __html: updText }} />;
+    }
+
+    return(
+        <>
+        <h2>Useful Information</h2>
+        <ul>
+            {usefulData.map((item, index) => (
+                <>
+                    <li key={index}>{renderTextWithInternalReferences(item.text, item.links)}</li>
+                    <hr/>
+                </>
+            ))}
+        </ul>
+        <br/><br/>
+        </>
+    );
+}
+
+export default UsefulInformation;
