c3891c39742ffa424ae8237d5b9f9e1d577f38d1
mnek1t <mednikita2004@gmail.com>
2025-03-30 02:26:06 +0200

    place UsefulInformation and Contact components to the page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,16 @@
 import Header from '../components/Header/Header';
 import Login from '../components/Login/Login';
+import UsefulInformation from '../components/UsefulInformation/UsefulInformation';
+import Contact from '../components/Contact/Contact';
 const LoginPage = () => {
     return(
         <div className="app-container">
             <Header />
             <hr/>
             <Login/>
+            <hr/>
+            <UsefulInformation/>
+            <Contact />
         </div>
     );
 }
