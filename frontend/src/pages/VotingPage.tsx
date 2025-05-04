04d31fbd509b1eb3af0b0940d897bae9170df6f0
mnek1t <mednikita2004@gmail.com>
2025-05-04 17:46:56 +0300

    display connected metamask account

--- frontend/src/pages/VotingPage.tsx
+++ frontend/src/pages/VotingPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header";
 import Contact from "../components/Contact/Contact";
 import { useState, useEffect } from "react";
+import { connectWallet } from "../api/blockchain/ethers";
 import StandardButton from "../components/StandardButton/StandardButton";
 type Candidate = {
     id: number;
@@ -8,12 +9,23 @@ type Candidate = {
     party: string;
     image: File | null;
     imagePreview: string;
-  };
+};
+
 const VotingPage = () => {
     const [candidates, setCandidates] = useState<Candidate[]>();
-    useEffect(() => {
+    const [connectedAccount, setConnectedAccount] = useState<string>();
 
+    useEffect(() => {
+        connectWallet()
+        .then((data) => setConnectedAccount(data))
+        .catch((err) => console.error(err))
     })
+
+    const handleConnectWallet = () => {
+        connectWallet()
+        .then((data) => setConnectedAccount(data))
+        .catch((err) => console.error(err))
+    } 
     return(
     <div className="app-container">
         <Header/>
@@ -22,8 +34,8 @@ const VotingPage = () => {
         <h6>This is Voter view. Please read instructions before voting</h6>
         <label htmlFor="acceptDeposit">Certify you are accept all policies and deposits:</label>
         <input id="acceptDeposit" type="checkbox"></input>
-        <h3>Metamask account connected: </h3>
-        <StandardButton label="Connect Metamask" className="button-blue" onClick={connectWalet}/>
+        <h3>Metamask account connected: {connectedAccount}</h3>
+        <StandardButton label="Connect Metamask" className="button-blue" onClick={handleConnectWallet}/>
         <h5>Voting will be finished on .. . ... left</h5>
         <form>
             <div>Select a candidate:</div>
