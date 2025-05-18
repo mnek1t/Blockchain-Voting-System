c4c4648a28c44591edfdf09d794916a94a7afe1e
mnek1t <mednikita2004@gmail.com>
2025-05-18 19:18:58 +0300

    display result page

--- frontend/src/pages/VotingResultPage.tsx
+++ frontend/src/pages/VotingResultPage.tsx
@@ -1,35 +1,47 @@
-
 import { useLocation, useNavigate } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Contact from '../components/Contact/Contact';
 import VotingBarChart from '../components/VoteBarChart/VoteBarChart';
 import { ElectionCandidatesResults } from '../types';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import ReferenceButton from '../components/ReferenceButton/ReferenceButton';
+import LoadingSpinner from '../components/LoadingSpinner/LoadingSpinner';
+
 const VotingResultPage = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const location = useLocation();
     const navigate = useNavigate();
-    const results = location.state?.results as ElectionCandidatesResults[];
+    const results = location.state?.results as ElectionCandidatesResults[] | undefined;
+
     useEffect(() => {
-        if(!results) {
-            navigate("/")
+        if (!results) {
+            navigate("/");
         }
-    }, [results])
-    return(
+    }, [results, navigate]);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setIsLoading(false);
+        }, 500);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
+    return (
         <div className="app-container">
             <Header />
-            <hr/>
-            <br/>
-            <button
-                onClick={() => navigate(-1)}
-                style={{ fontFamily:"sans-serif", fontSize: '15px',background: 'none', border: 'none', color: '#012169', textDecoration: 'underline', cursor: 'pointer', padding: 0 }}
-            >
-                &lt; To home page
-            </button>
-            <br/><br/>
-            {results ? <VotingBarChart results={results} /> : <p>Loading...</p>}
+            <hr />
+            <br />
+            <ReferenceButton label='To home page'/>
+            <br /><br />
+            {isLoading ? (
+                <LoadingSpinner innertText="Loading Results..." loading={true} />
+            ) : (
+                results && <VotingBarChart results={results} />
+            )}
             <Contact />
         </div>
     );
-}
+};
 
 export default VotingResultPage;
